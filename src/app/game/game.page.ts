24ba4452c401c1ccdf96e0415c7cb00afa7e7178
Ragnaars <orl.tobar@duocuc.cl>
2023-05-24 19:18:05 -0400

    Cree formulario para enviar respuesta

--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiService} from "../service/api.service";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-game',
@@ -10,22 +11,41 @@ export class GamePage implements OnInit {
 
   //variables
   questions : any = [];
+  questionTitle: any;
+  questionAnswer: any;
+  helpAnswer: any;
+  questionNumber : any;
+  formSendAnswer: FormGroup ;
   constructor(
+    public formBuilder: FormBuilder,
     private serviceApi : ApiService
-  ) { }
+  ) { 
+      this.formSendAnswer = this.formBuilder.group({
+        answer : ["",Validators.required]
+      })
+    }
 
   ngOnInit() {
     this.getQuestions();
+    this.questionNumber = 0;
   }
 
   getQuestions(){
     this.serviceApi.getQuestions().subscribe(
       (res : any ) =>{
         this.questions = res;
+        this.questionTitle = this.questions[0].question;
+        this.questionAnswer = this.questions[0].answer;
+        this.helpAnswer = this.questions[0].help;
         console.log(this.questions);
       }, (err:any) =>{
         console.log("error : ",err)
       }
     );
+  }
+
+  sendAnswer(){
+    let value = this.formSendAnswer.value;
+    console.log("Respuesta Enviada",value)
   }
 }
